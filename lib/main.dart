0f86bcde5efd5435b1f72f27078683b38bed26f9
dinendra007 <dinendradb@gmail.com>
2020-03-30 14:50:21 +0530

    added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightnum = 1;
+  int leftnum = 1;
+  void changenum() {
+    setState(() {
+      leftnum = Random().nextInt(6) + 1;
+      rightnum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changenum();
+                },
+                child: Image.asset('images/dice$leftnum.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changenum();
+                },
+                child: Image.asset('images/dice$rightnum.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
